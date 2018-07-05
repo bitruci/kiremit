f6f9f23bed4a0a14f2088598350782dfd30c4104
omer.karaca <omer.karaca@bulutyazilim.com>
2018-07-05 20:45:50 +0300

    add dependencies

--- app/src/main/java/com/bitruci/kiremit/MainActivity.java
+++ app/src/main/java/com/bitruci/kiremit/MainActivity.java
@@ -1,8 +1,7 @@
 package com.bitruci.kiremit;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
